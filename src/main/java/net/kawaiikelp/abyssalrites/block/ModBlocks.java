12c8bfaaac5983f6ab607799fb7cfcf11bbd4664
KawaiiKelp <plekcontact12@gmail.com>
2024-11-07 15:47:05 +0900

    심연블록만들음

---
+++ src/main/java/net/kawaiikelp/abyssalrites/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.kawaiikelp.abyssalrites.block;
+
+import net.kawaiikelp.abyssalrites.AbyssalRites;
+import net.kawaiikelp.abyssalrites.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, AbyssalRites.MOD_ID);
+
+    public static final RegistryObject<Block> ABYSS_BLOCK = registerBlock("abyss_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.SCULK).sound(SoundType.SCULK)));
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
--- src/main/java/net/kawaiikelp/abyssalrites/item/ModCreativeModeTabs.java
+++ src/main/java/net/kawaiikelp/abyssalrites/item/ModCreativeModeTabs.java
@@ -1,6 +1,7 @@
 package net.kawaiikelp.abyssalrites.item;
 
 import net.kawaiikelp.abyssalrites.AbyssalRites;
+import net.kawaiikelp.abyssalrites.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -13,14 +14,16 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AbyssalRites.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> ABYSSAL_RITES_ITEMS = CREATIVE_MODE_TABS.register("abyssal_rites_items",
+    public static final RegistryObject<CreativeModeTab> ABYSSAL_RITES = CREATIVE_MODE_TABS.register("abyssal_rites",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ABYSS_SHARD.get()))
-                    .title(Component.translatable("creativetab.abyssal_rites_items"))
+                    .title(Component.translatable("creativetab.abyssal_rites"))
                     .displayItems((pParameters, pOutput) -> {
-                        pOutput.accept(ModItems.DIAMANTE.get()); // Abyssal Rites: Items 탭에 Diamante 아이템 등록
-                        pOutput.accept(ModItems.ABYSS_SHARD.get()); // Abyssal Rites: Items 탭에 Abyss Shard 아이템 추가
+                        pOutput.accept(ModItems.DIAMANTE.get()); // Abyssal Rites 탭에 Diamante 아이템 등록
+                        pOutput.accept(ModItems.ABYSS_SHARD.get()); // Abyssal Rites 탭에 Abyss Shard 아이템 추가
+
+                        pOutput.accept(ModBlocks.ABYSS_BLOCK.get()); // Abyssal Rites 탭에 Abyss Block 블록 추가
                     })
-                    .build()); // Abyssal Rites: Items 탭 등록
+                    .build()); // Abyssal Rites 탭 등록
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
